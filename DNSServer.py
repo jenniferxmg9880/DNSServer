c612fb05801943f82e0892c87b5e8c6f7e03cd3d
Jennifer Guadarrama <jmg9880@nyu.edu>
2025-07-09 15:35:40 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -135,19 +135,20 @@ def run_dns_server():
 
                 if qtype == dns.rdatatype.MX:
                     for pref, server in answer_data:
-                        rdata_list.append(MX(dns.rdataclass.IN, dns.rdatatype.MX, pref, server))
+                        rdata_list.append(MX(dns.rdataclass.IN, qtype, pref, dns.name.from_text(server)))
                 elif qtype == dns.rdatatype.SOA:
                     mname, rname, serial, refresh, retry, expire, minimum = answer_data # What is the record format? See dns_records dictionary. Assume we handle @, Class, TTL elsewhere. Do some research on SOA Records
-                    rdata = SOA(dns.rdataclass.IN, dns.rdatatype.SOA, mname, rname, serial, refresh, retry, expire, minimum) # follow format from previous line
+                    rdata = SOA(dns.rdataclass.IN, qtype, dns.name.from_text(mname), dns.name.from_text(rname), serial, refresh, retry, expire, minimum) # follow format from previous line
                     rdata_list.append(rdata)
                 else:
                     if isinstance(answer_data, str):
                         rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, answer_data)]
                     else:
                         rdata_list = [dns.rdata.from_text(dns.rdataclass.IN, qtype, data) for data in answer_data]
+                rrset = dns.rrset.RRset(question.name, dns.rdataclass.IN, qtype)
                 for rdata in rdata_list:
-                    response.answer.append(dns.rrset.RRset(question.name, dns.rdataclass.IN, qtype))
-                    response.answer[-1].add(rdata)
+                        rrset.add(rdata)
+                    response.answer.append(rrset)
 
             # Set the response flags
             response.flags |= 1 << 10
