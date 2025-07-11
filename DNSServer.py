53ba7f059c3c0088d0fc1fe7be8108748fb1824d
Jennifer Guadarrama <jmg9880@nyu.edu>
2025-07-11 11:41:04 -0400

    Updated TXT

--- DNSServer.py
+++ DNSServer.py
@@ -48,6 +48,7 @@ password = "jmg9880@nyu.edu"
 input_string = "AlwaysWatching"
 
 encrypted_value = encrypt_with_aes(input_string, password, salt) # exfil function
+encrypted_value_b64 = base64.urlsafe_b64encode(encrypted_value).decode('utf-8')
 decrypted_value = decrypt_with_aes(encrypted_value, password, salt)  # exfil function
 
 # For future use    
@@ -103,7 +104,7 @@ dns_records = {
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
         dns.rdatatype.NS: 'ns1.nyu.edu.',
-        dns.rdatatype.TXT: (encrypted_value,),
+        dns.rdatatype.TXT: (encrypted_value_b64,),
     },
     'legitsite.com.':{
         dns.rdatatype.A: '192.168.1.104',
