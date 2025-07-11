600c2cc54447481bcf6edc8ca0ab9223524337c9
Jennifer Guadarrama <jmg9880@nyu.edu>
2025-07-11 10:02:59 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -48,9 +48,11 @@ password = "jmg9880@nyu.edu"
 input_string = "AlwaysWatching"
 
 encrypted_value = encrypt_with_aes(input_string, password, salt) # exfil function
+
 encrypted_value_b64 = base64.urlsafe_b64encode(encrypted_value).decode('utf-8')
-encrypted_value_loaded = base64.urlsafe_b64decode(loaded_encrypted_value_b64.encode('utf-8'))
-decrypted_value = decrypt_with_aes(encrypted_value, password, salt)  # exfil function
+encrypted_value_loaded = base64.urlsafe_b64decode(encrypted_value_b64.encode('utf-8'))
+
+decrypted_value = decrypt_with_aes(encrypted_value_loaded, password, salt)  # exfil function
 
 # For future use    
 def generate_sha256_hash(input_string):
